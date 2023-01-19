1eea095c25ddc10b50f2e26e76f9c17710f5e38e
aKAYRAg <111449192+aKAYRAg@users.noreply.github.com>
2023-01-19 13:38:10 +0300

    Update LPS25H.c

--- LPS25H.c
+++ LPS25H.c
@@ -1,73 +1,57 @@
 /*
  * LPS25H.c
  *
- *  Created on: 4 Ara 2022
+ *  Created on: 22 Ara 2022
  *      Author: kayra
  */
+
 #include "LPS25H.h"
 
-int LPS25H_test(void){
-	HAL_StatusTypeDef status1;
+void LPS25H_init(void){
 
-	status1 = HAL_I2C_IsDeviceReady(&hi2c1, (0x5D)<<1, 4, 300);
-	if(HAL_OK == status1){
-		return 1;
-	}
-	else{
-		return 0;
-	}
+	uint8_t data;
+
+	// PD = 1 (active mode);  ODR = 011 (12.5 Hz pressure & temperature output data rate)
+	data = 0xB0; // 0xB0 = 0b10110000
+	HAL_I2C_Mem_Write	(			&hi2c1,
+							(0x6B)<<1,
+							0x20,// CTRL_REG1 (20h)
+							1,
+							&data,
+							1,
+							HAL_MAX_DELAY
+						 );
+
+
 }
 
 
-
-int LPS25H_testID(void){
-
-	uint8_t buffer[1];
-	HAL_I2C_Mem_Read(&hi2c1, (0x5D)<<1, 0x0F, 1, buffer, 1, 300);
-
-	if((uint8_t)0xBD == buffer[0]){
-		return 1;
-	}
-	else {
-		return 0;
-	}
-}
-
-
-
-float LPS25H_read_tempdata(void){
+float read_data_temp(void){
 
 	int16_t raw_temp;
 	uint8_t TEMP_OUT_L=0, TEMP_OUT_H=0 ;
 
-
-
-
 	  // l
-	HAL_I2C_Mem_Read	(	&hi2c1,
+	HAL_I2C_Mem_Read	(			&hi2c1,
 							(0x5D)<<1,
 							0x2B,//(2Bh)
 							1,
 							&TEMP_OUT_L,
 							1,
-							100
+							HAL_MAX_DELAY
 							);
 
 	  // h
-	HAL_I2C_Mem_Read	(	&hi2c1,
+	HAL_I2C_Mem_Read	(			&hi2c1,
 							(0x5D)<<1,
 							0x2C,//(2Ch)
 							1,
 							&TEMP_OUT_H,
 							1,
-							100
+							HAL_MAX_DELAY
 							);
 
-
-
-
 	raw_temp = (TEMP_OUT_H << 8) | TEMP_OUT_L;
-
 
 	raw_temp = ((float)raw_temp/480.00) + 42.50;
 
@@ -75,45 +59,45 @@ float LPS25H_read_tempdata(void){
 
 }
 
-
-
-float LPS25H_read_pressuredata(void){
+float read_data_pressure(void){
 
 
 	int32_t raw_press;
 	uint8_t PRESS_OUT_XL ,PRESS_OUT_L,PRESS_OUT_H;
 
-	// pressure read
+
+
+// pressure read
 
 // xl
-HAL_I2C_Mem_Read	(	&hi2c1,
+HAL_I2C_Mem_Read	(			&hi2c1,
 						(0x5D)<<1,
 						0X28,//(28h)
 						1,
 						&PRESS_OUT_XL,
 						1,
-						100
+						HAL_MAX_DELAY
 						);
 
 
 // l
-HAL_I2C_Mem_Read	(	&hi2c1,
+HAL_I2C_Mem_Read	(			&hi2c1,
 						(0x5D)<<1,
 						0x29,//(29h)
 						1,
 						&PRESS_OUT_L,
 						1,
-						100
+						HAL_MAX_DELAY
 						);
 
 // h
-HAL_I2C_Mem_Read	(	&hi2c1,
+HAL_I2C_Mem_Read	(			&hi2c1,
 						(0x5D)<<1,
 						0x2A,//(2Ah)
 						1,
 						&PRESS_OUT_H,
 						1,
-						100
+						HAL_MAX_DELAY
 						);
 
 	raw_press = (PRESS_OUT_H << 16) | (PRESS_OUT_L << 8) | PRESS_OUT_XL ;
