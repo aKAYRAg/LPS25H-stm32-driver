992ce9afe88adfbbd0aebb83d7929b3c06a64633
aKAYRAg <111449192+aKAYRAg@users.noreply.github.com>
2023-01-19 13:30:41 +0300

    Update LPS25H.h

--- LPS25H.h
+++ LPS25H.h
@@ -1,26 +1,24 @@
 /*
  * LPS25H.h
  *
- *  Created on: 4 Ara 2022
+ *  Created on: 22 Ara 2022
  *      Author: kayra
  */
+
+#include "stm32f4xx_hal.h"
+#include "stdint.h"
+#include <math.h>
 
 #ifndef INC_LPS25H_H_
 #define INC_LPS25H_H_
 
-#include "stm32f4xx_hal.h"
-
 extern I2C_HandleTypeDef hi2c1;
 
-int LPS25H_test(void);
+void LPS25H_init(void);
+
+float read_data_temp(void);
+float read_data_pressure(void);
+
 int LPS25H_testID(void);
-
-float LPS25H_read_tempdata(void);
-float LPS25H_read_pressuredata(void);
-
-
-
-
-
 
 #endif /* INC_LPS25H_H_ */
